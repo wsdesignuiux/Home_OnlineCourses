e976ff7a80c3b02b988204a11b48f06c62faf897
Rushabh Patel <wsdesignuiux@gmail.com>
2018-11-17 12:04:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vaibhav/home_onlinecourses/Home_OnlineCoursesActivity.java
@@ -0,0 +1,13 @@
+package com.example.vaibhav.home_onlinecourses;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home_OnlineCoursesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home__online_courses);
+    }
+}
